45e3e08147e67586b8352f34764a8f7f8f278e9b
dinaldi21 <dwikidinaldi@gmail.com>
2021-09-22 15:47:41 +0700

    tester

---
+++ commands/leave.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'leave',
+    decsription: 'leave channel',
+    async execute(message, args){
+        const VoiceChannel = message.member.voice.channel;
+        if(!VoiceChannel) return message.channel.send('Get In the Voice Channel');
+        await VoiceChannel.leave();
+        await message.channel.send('Leaving');
+    }
+}
